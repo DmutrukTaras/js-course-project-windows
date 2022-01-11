5f484739663df44af3fa8c7ee85687d797603d54
Taras <dmutruk.taras@gmail.com>
2022-01-11 17:50:49 +0200

    modal windows

---
+++ src/js/modules/tabs.js
@@ -0,0 +1,14 @@
+const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+    let header = document.querySelector(headerSelector),
+        tabs = document.querySelectorAll(tabSelector),
+        content = document.querySelectorAll(contentSelector);
+
+
+
+
+
+}
+
+
+
+export default tabs;
